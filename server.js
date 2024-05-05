e4f3079539874773b55970d437ae615ba1f738e2
shammaskhann <shammas.csgo0312@gmail.com>
2024-05-05 12:15:11 -0700

    deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express(); 
 const port = 3000; 
 const sql = require('mssql'); 
+const { DefaultAzureCredential } = require('@azure/identity');
 const customers = require('./src/api/Customers'); 
 const bodyParser = require('body-parser'); 
 // Use body-parser middleware
