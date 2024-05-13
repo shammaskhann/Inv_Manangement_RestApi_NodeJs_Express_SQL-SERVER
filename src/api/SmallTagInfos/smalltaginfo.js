87d1aea2d5a8921321d78c1887c3a51a63dfb957
shammaskhann <shammas.csgo0312@gmail.com>
2024-05-13 21:13:31 +0500

    test prob v1.2

--- src/api/SmallTagInfos/smalltaginfo.js
+++ src/api/SmallTagInfos/smalltaginfo.js
@@ -25,17 +25,17 @@ router.get('/getTotalSalesToday',async (req,res) => {
 }
 );
 
-// // get Total Revenue
-// router.get('/getTotalRevenue',async (req,res) => {
-//     const pool = req.app.get('dbPool');
-//     try{
-//         const result= await pool.request().execute("dbo.getTotalRevenue");
-//         res.status(200).send(result.recordset);
-//     }catch(err){
-//         res.status(500).send({message: err.message});
-//     }
-// }
-// );
+// get Total Revenue
+router.get('/getTotalRevenue',async (req,res) => {
+    const pool = req.app.get('dbPool');
+    try{
+        const result= await pool.request().execute("dbo.getTotalRevenue");
+        res.status(200).send(result.recordset);
+    }catch(err){
+        res.status(500).send({message: err.message});
+    }
+}
+);
 
 // // get Total Products
 // router.get('/getTotalProducts',async (req,res) => {
