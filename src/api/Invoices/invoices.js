d4b25f753b084f44f41ce9f552e95073d6d000cc
shammaskhann <shammas.csgo0312@gmail.com>
2024-05-24 20:08:33 +0500

    prod live v1.9

--- src/api/Invoices/invoices.js
+++ src/api/Invoices/invoices.js
@@ -23,4 +23,25 @@ router.get('/getInvoices', async (req, res) => {
     }
 }
 );
+
+router.get('/getInvoice/:id', async (req, res) => {
+    const pool = new sql.ConnectionPool(config, err => {
+        if (err) {
+            console.log("Error while connecting to database :- " + err);
+            throw err;
+        }
+        console.log("Connection Successful!");
+    });
+    await pool.connect();
+    try {
+        const result = await pool.request().input('id', req.params.id).execute("dbo.invoicebypoid");
+        res.status(200).send(result.recordset);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    } finally {
+        await pool.close();
+
+    }
+}
+);
 module.exports = router;
