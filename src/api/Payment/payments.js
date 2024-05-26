02e90559adfddc8df246caa6a46111a879c3769e
shammaskhann <shammas.csgo0312@gmail.com>
2024-05-27 01:10:12 +0500

    prod live v2.1

--- src/api/Payment/payments.js
+++ src/api/Payment/payments.js
@@ -25,5 +25,26 @@ router.get('/getPayment/:id', async (req, res) => {
     }
 }
 );
+//EXEC UpdatePaymentStatus @OrderID = 32, @NewStatus = 'Paid';  
+router.put('/updatePaymentStatus/:id', async (req, res) => {
+    const pool = new sql.ConnectionPool(config, err => {
+        if (err) {
+            console.log("Error while connecting to database :- " + err);
+            throw err;
+        }
+        console.log("Connection Successful!");
+    });
+    await pool.connect();
+    try {
+        const result = await pool.request().input('OrderID', req.params.id).input('NewStatus', req.body.NewStatus).execute("dbo.UpdatePaymentStatus");
+        res.status(200).send(result.recordset);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    } finally {
+        await pool.close();
+
+    }
+}
+);
 
 module.exports = router;
